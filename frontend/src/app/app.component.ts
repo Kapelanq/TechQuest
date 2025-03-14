218530bb3a357a4c93d558653cbfa7b0956647c9
Kapelanq <pixel131513@gmail.com>
2025-03-14 11:26:09 +0100

    ai

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import Anthropic from "@anthropic-ai/sdk";
+import { GoogleGenerativeAI } from '@google/generative-ai'
 
 @Component({
   selector: 'app-root',
@@ -12,16 +12,16 @@ import Anthropic from "@anthropic-ai/sdk";
 export class AppComponent {
   title = 'TechQuest';
 
-const anthropic = new Anthropic({
-  // defaults to process.env["ANTHROPIC_API_KEY"]
-  apiKey: "my_api_key",
-});
+   genAI = new GoogleGenerativeAI("AIzaSyBQAC-cjYyQWmX0F1FfzyHrVX9Erhng3ks");
+   model = this.genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const msg = await anthropic.messages.create({
-  model: "claude-3-7-sonnet-20250219",
-  max_tokens: 20000,
-  temperature: 1,
-  messages: []
-});
-console.log(msg);
+   prompt = "What is 2+2?";
+
+   result = this.model.generateContent(this.prompt);
+  
+
+ngOnInit(): void{
+  console.log(this.result);
+}
+
 }
