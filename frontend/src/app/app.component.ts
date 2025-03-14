d7375e8ba4a58d1e670210d604b1379b0a99e914
Kapelanq <pixel131513@gmail.com>
2025-03-14 11:04:57 +0100

    tests

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Anthropic from "@anthropic-ai/sdk";
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,18 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'y';
+  title = 'TechQuest';
+
+const anthropic = new Anthropic({
+  // defaults to process.env["ANTHROPIC_API_KEY"]
+  apiKey: "my_api_key",
+});
+
+const msg = await anthropic.messages.create({
+  model: "claude-3-7-sonnet-20250219",
+  max_tokens: 20000,
+  temperature: 1,
+  messages: []
+});
+console.log(msg);
 }
